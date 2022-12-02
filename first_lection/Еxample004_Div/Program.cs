9a90e09845ec490d7f399e9a45a9c4f328757d7e
iYannis <skalvolin@yandex.ru>
2022-12-02 17:13:46 +0300

    add random

---
+++ first_lection/Еxample004_Div/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа деления  двух чисел
+
+double numberA = new Random().Next(1,100);
+double numberB = new Random().Next(1,10);
+double div = numberA / numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(div);
+double ostatok=numberA%10;
+Console.WriteLine(ostatok);
