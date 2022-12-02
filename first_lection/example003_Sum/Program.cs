0f0c36e43dde0bc6b5480a68650eb5b8fdee1fd1
iYannis <skalvolin@yandex.ru>
2022-12-02 16:47:09 +0300

    example 003 sum

---
+++ first_lection/example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа для нахождения суммы двух чисел
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int sum = a + b;
+Console.WriteLine(sum); 
