33a61e5439663ce556eb0b8ad402300c0e55068d
iYannis <skalvolin@yandex.ru>
2022-12-02 18:15:00 +0300

    final first lection commit

---
+++ first_lection/Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿// если пришел Хантер то для особое приветствие
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "хантер")
+{
+Console.WriteLine("О, да это же ХАНТЕР!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
---
+++ first_lection/Example006_Base/Program.cs
@@ -0,0 +1,19 @@
+﻿// задача про гири
+int a = new Random().Next(1,100);
+int b = new Random().Next(1,100);
+int c = new Random().Next(1,100);
+int d = new Random().Next(1,100);
+int e = new Random().Next(1,100);
+int max = a;
+if(a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;
+if(d>max) max = d;
+if(e>max) max = e;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.Write("max = ");
+Console.WriteLine(max);
