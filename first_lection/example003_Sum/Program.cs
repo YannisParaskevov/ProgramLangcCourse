c0649ae298c75c593aebd8984fc708614be922c0
iYannis <skalvolin@yandex.ru>
2022-12-02 16:52:18 +0300

    example 003 sum plus

--- first_lection/example003_Sum/Program.cs
+++ first_lection/example003_Sum/Program.cs
@@ -5,3 +5,7 @@ Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine());
 int sum = a + b;
 Console.WriteLine(sum); 
+int numberA = 33;
+int numberB = 67;
+int sum1 = numberA + numberB;
+Console.WriteLine(sum1);
