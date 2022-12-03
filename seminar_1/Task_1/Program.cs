b8040ddca0cf9dcb676b4f1037fdbb39b4d1d541
iYannis <skalvolin@yandex.ru>
2022-12-03 17:14:40 +0300

    tasks 2 and 4 done

--- seminar_1/Task_1/Program.cs
+++ seminar_1/Task_1/Program.cs
@@ -3,9 +3,15 @@ Console.Write("a:");
 int a = int.Parse(Console.ReadLine());
 Console.Write("b:");
 int b = int.Parse(Console.ReadLine());
-if(Math.Pow(b,2) == a){
+// if(Math.Pow(b,2) == a){
+//    Console.WriteLine("а квадрат b");
+// }else{
+//    Console.WriteLine("а не квадрат b");
+// }
+// Console.WriteLine($"{Math.Pow(b,2) == a}");
+
+if(b*b == a){
     Console.WriteLine("а квадрат b");
 }else{
     Console.WriteLine("а не квадрат b");
 }
-Console.WriteLine($"{Math.Pow(b,2) == a}");
