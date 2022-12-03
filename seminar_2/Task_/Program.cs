a002bf7165fc39be8720578fd73293de39155c0f
iYannis <skalvolin@yandex.ru>
2022-12-03 21:24:53 +0300

    after 2nd seminar

--- seminar_2/Task_/Program.cs
+++ seminar_2/Task_/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Clear();
+﻿// задача 12. кратно ли второе число первому и если нет, то какой остаток
+Console.Clear();
 Console.Write("Введите число 1: ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите число 2: ");
---
+++ seminar_2/Task_14/Program.cs
@@ -0,0 +1,17 @@
+﻿// принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+// Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int even7 = num % 7;
+int even23 = num % 23;
+Console.WriteLine($"{even7} {even23}");
+if(even7 == 0 && even23 == 0) 
+{
+    Console.WriteLine("да, кратно");
+}
+else
+{
+Console.WriteLine("не кратно");
+Console.WriteLine($"Остаток деления на 7 = {even7}");
+Console.WriteLine($"Остаток деления на 23 = {even23}");
+}
