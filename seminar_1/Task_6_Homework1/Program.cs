778ca635f1dea80f3b1e83f5cf52dba96177d06f
iYannis <skalvolin@yandex.ru>
2022-12-03 19:35:46 +0300

    homework 1 final

--- seminar_1/Task_6_Homework1/Program.cs
+++ seminar_1/Task_6_Homework1/Program.cs
@@ -3,11 +3,11 @@ Console.Clear();
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 int even = number%2;
-if(even > 0)
+if(even == 0)
 {
-    Console.WriteLine($"Нет, число {number} нечетное");
+    Console.WriteLine($"Да, число {number} четное");
 } 
 else
 {
-    Console.WriteLine($"Да, число {number} четное");
+    Console.WriteLine($"Нет, число {number} нечетное");
 }
